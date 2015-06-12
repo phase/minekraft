074a6260de27d207134f5d07acfaa6316be9323b
Phase <ph4sesaber@gmail.com>
2015-06-12 04:51:43 -0700

    All of the languages

---
+++ src/main/groovy/xyz/jadonfowler/minekraft/launcher/Launcher.groovy
@@ -0,0 +1,9 @@
+package xyz.jadonfowler.minekraft.launcher;
+
+import xyz.jadonfowler.minekraft.Minekraft;
+
+class Launcher {
+    static void main(String[] args) {
+        //TODO Create launcher with tumbler page
+    }
+}
---
+++ src/main/scala/xyz/jadonfowler/minekraft/Something.scala
@@ -0,0 +1,6 @@
+object Something {
+    //No idea what to do with Scala, but we can compile it (last)!
+    def main(String[] args){
+        println("Why am I being called?");
+    }
+}
