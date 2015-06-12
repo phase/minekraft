4191989a4cebc2a65f7fb54f2a0a2681e156201f
phase <ph4sesaber@gmail.com>
2015-06-12 05:54:59 -0700

    Basic JFrame

--- src/main/groovy/xyz/jadonfowler/minekraft/launcher/Launcher.groovy
+++ src/main/groovy/xyz/jadonfowler/minekraft/launcher/Launcher.groovy
@@ -1,9 +1,24 @@
 package xyz.jadonfowler.minekraft.launcher;
 
 import xyz.jadonfowler.minekraft.Minekraft;
+import javax.swing.*
 
 class Launcher {
     static void main(String[] args) {
         //TODO Create launcher with tumbler page
+        JEditorPane jep = new JEditorPane();
+        jep.setEditable(false);
+        try{
+            jep.setPage("https://github.com/phase/minekraft");
+        } catch (IOException e){
+            jep.setContentType("text/html");
+            jep.setText("<html><h1>Minekraft</h1><p>There was an error loading the page!</p></html>");
+        }
+        JScrollPane scrollPane = new JScrollPane(jep);     
+        JFrame f = new JFrame("Minekraft Launcher");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.getContentPane().add(scrollPane);
+        f.setPreferredSize(new Dimension(800,600));
+        f.setVisable(true);
     }
 }
